1a269e011666cdead2c131bf88c91a379ce001c6
rojiloro <kelengoji79@gmail.com>
2023-07-06 23:04:35 +0700

    user done

--- repositories/user-repositories.go
+++ repositories/user-repositories.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"LandTicket-Backend/models"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -10,8 +9,7 @@ import (
 type UserRepository interface {
 	FindUser() ([]models.User, error)
 	GetUser(ID int) (models.User, error)
-	CreateUser(user models.User) (models.User, error)
-	UpdateUser(user models.User, ID int) (models.User, error)
+	
 }
 
 type repository struct {
@@ -36,14 +34,3 @@ func (r *repository) GetUser(ID int) (models.User, error){
 	return user, err
 }
 
-func (r *repository) CreateUser (user models.User) (models.User, error){
-	err := r.db.Exec("INSERT INTO users(fullname, username, email, password, created_at, updated_at) VALUES (?,?,?,?,?,?)", user.Fullname, user.Username, user.Email, user.Password, time.Now(), time.Now()).Error
-
-	return user, err
-}
-
-func (r *repository) UpdateUser (user models.User, ID int)(models.User, error){
-	err := r.db.Raw("UPDATE users SET fullname=?, username=?, email=?, password=? WHERE id=?", user.Fullname, user.Username, user.Email, user.Password,ID).Scan(&user).Error
-
-	return user, err
-}
