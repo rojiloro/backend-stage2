cfa4ad60bb4e018843257156c0f0b201454991cd
rojiloro <kelengoji79@gmail.com>
2023-07-08 21:56:20 +0700

    get all jadi ges

--- repositories/ticket-repositories.go
+++ repositories/ticket-repositories.go
@@ -24,14 +24,14 @@ func (r *repository) CreateTicket(ticket models.Ticket)(models.Ticket, error){
 
 func (r *repository) FindTicket()([]models.Ticket, error){
 	var ticket []models.Ticket
-	err := r.db.Preload("Station").Find(&ticket).Error
+	err := r.db.Preload("StartStation").Preload("DestinationStation").Find(&ticket).Error
 
 	return ticket, err
 }
 
 func (r *repository) GetTicket(ID int)(models.Ticket, error){
 	var ticket models.Ticket
-	err := r.db.Preload("Station").First(&ticket, ID).Error
+	err := r.db.Preload("Station").Preload("DestinationStation").First(&ticket, ID).Error
 
 	return ticket, err
 }
